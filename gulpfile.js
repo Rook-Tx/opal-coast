82d82b786cd5bc747c12d4a96a74be0fb2b842eb
Tom Corbett <twcorb@gmail.com>
2016-08-04 09:08:53 -0400

    1607 update

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,21 @@
-var errorHandler, gulp, stylus, autoprefixer, sourcemaps, del;
 
-gulp = require('gulp'),
-stylus = require('gulp-stylus'),
-autoprefixer = require('gulp-autoprefixer'),
-sourcemaps = require('gulp-sourcemaps'),
-del = require('del');
+// ----------------------------------------
+// Include CoffeeScript Compiler
+// ----------------------------------------
+require('coffee-script/register');
 
-errorHandler = function(error) {
-  return console.log('Task Error: ', error);
-};
+// ----------------------------------------
+// Require Gulp Utilities
+// ----------------------------------------
+var util = require('gulp-util');
 
-gulp.task('stylus', function () {
-  return gulp.src('stylus/*.styl')
-    .pipe(sourcemaps.init())
-    .pipe(stylus({
-      compress: true
-    }))
-    .pipe(autoprefixer('last 3 version'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('css'));
-});
+// ----------------------------------------
+// Log our use of a different Gulpfile
+// ----------------------------------------
+var gulpfile = 'gulpfile.coffee';
+util.log('Using file', util.colors.magenta(gulpfile));
 
-gulp.task('watch', function() {
-  gulp.watch('stylus/**/*.styl', ['stylus']).on('error', errorHandler);
-});
-
-gulp.task('clean', function(cb) {
-    del(['css'], cb)
-});
-
-gulp.task('default', ['watch']);
+// ----------------------------------------
+// Include our Gulpfile.coffee
+// ----------------------------------------
+require('./' + gulpfile);
